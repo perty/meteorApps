760dc07eeec940679af9d457d836c95386c69a6f
Per Lundholm <per@artcomputer.se>
2016-06-06 08:30:21 +0200

    1.3  "Collections", third  but broken commit of tutorial. Tasks.find does not return anything in body.js

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,12 +1,13 @@
 
 import { Template } from 'meteor/templating';
 
+import { Tasks } from '../api/tasks.js'
+
 import './body.html';
 
 Template.body.helpers({
-    tasks: [
-        { text: 'This is task 1' },
-        { text: 'This is task 2' },
-        { text: 'This is task 3' },
-    ]
+    tasks() {
+        //return Tasks.find({});
+        return [ {text: 'begin'} , Tasks.find({}), {text: 'end'}]
+    }
 });
