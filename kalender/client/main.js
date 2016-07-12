6bec206a7dc714578ecf0e0b1fdd597ba797d8bf
Per Lundholm <per@artcomputer.se>
2016-07-12 18:44:10 +0200

    First steps

---
+++ kalender/client/main.js
@@ -0,0 +1,44 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+    // counter starts at 0
+    this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+    counter() {
+        return Template.instance().counter.get();
+    },
+});
+
+Template.hello.events({
+    'click button'(event, instance) {
+        // increment the counter when button is clicked
+        instance.counter.set(instance.counter.get() + 1);
+    },
+});
+
+Template.weekdays.helpers(
+    {
+        wdays() {
+            return ['M', 'T', 'O', 'T', 'F']
+        }
+    }
+);
+
+Template.kalendar.helpers(
+    {
+        kalendarRows() {
+            return [
+                [" ", 2, 3, 4, 5],
+                [8, 9, 10, 11, 12]
+            ]
+        },
+        wdays() {
+            return [null, 'M', 'T', 'O', 'T', 'F', null]
+        }
+    }
+)
